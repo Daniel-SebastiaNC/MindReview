fb582f09004dacf92567377c816bcd329a79dc0f
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-21 14:52:27 -0300

    first commit

---
+++ src/main/java/br/com/dev/danielsebastian/MindReview/MindReviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.danielsebastian.MindReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/danielsebastian/MindReview/MindReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.danielsebastian.MindReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
