23bc9d27aabbe61c1f5e5552670eebc613b81050
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-23 01:07:44 -0300

    feat: add endpoint GET to GetAllQuestion in QuestionController

--- src/main/java/br/com/dev/danielsebastian/MindReview/infra/presentation/QuestionController.java
+++ src/main/java/br/com/dev/danielsebastian/MindReview/infra/presentation/QuestionController.java
@@ -2,15 +2,15 @@ package br.com.dev.danielsebastian.MindReview.infra.presentation;
 
 import br.com.dev.danielsebastian.MindReview.core.domians.Question;
 import br.com.dev.danielsebastian.MindReview.core.usecases.CreateQuestionUsecase;
+import br.com.dev.danielsebastian.MindReview.core.usecases.GetAllQuestionUsecase;
 import br.com.dev.danielsebastian.MindReview.infra.dtos.QuestionDto;
 import br.com.dev.danielsebastian.MindReview.infra.mappers.QuestionDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
     private final CreateQuestionUsecase createQuestionUsecase;
+    private final GetAllQuestionUsecase getAllQuestionUsecase;
     private final QuestionDtoMapper questionDtoMapper;
 
     @PostMapping("/add")
@@ -25,5 +26,14 @@ public class QuestionController {
         Question question = questionDtoMapper.toDomain(questionDto);
         Question saveQuestion = createQuestionUsecase.execute(question);
         return ResponseEntity.status(HttpStatus.CREATED).body(questionDtoMapper.toDto(saveQuestion));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<QuestionDto>> getAllQuestion(){
+        return ResponseEntity.ok(
+                getAllQuestionUsecase.execute().stream()
+                        .map(questionDtoMapper::toDto)
+                        .toList()
+        );
     }
 }
