3946a27628fd942770e8f322bb7efc972df6fac6
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-23 01:05:17 -0300

    feat(bean): add Bean to GetAllQuestionUsecase

--- src/main/java/br/com/dev/danielsebastian/MindReview/infra/beans/BeanConfiguration.java
+++ src/main/java/br/com/dev/danielsebastian/MindReview/infra/beans/BeanConfiguration.java
@@ -3,6 +3,8 @@ package br.com.dev.danielsebastian.MindReview.infra.beans;
 import br.com.dev.danielsebastian.MindReview.core.gateway.QuestionGateway;
 import br.com.dev.danielsebastian.MindReview.core.usecases.CreateQuestionUsecase;
 import br.com.dev.danielsebastian.MindReview.core.usecases.CreateQuestionUsecaseImpl;
+import br.com.dev.danielsebastian.MindReview.core.usecases.GetAllQuestionUsecase;
+import br.com.dev.danielsebastian.MindReview.core.usecases.GetAllQuestionUsecaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class BeanConfiguration {
     @Bean
     public CreateQuestionUsecase createQuestionUsecase(QuestionGateway questionGateway){
         return new CreateQuestionUsecaseImpl(questionGateway);
+    }
+
+    @Bean
+    public GetAllQuestionUsecase getAllQuestionUsecase(QuestionGateway questionGateway){
+        return new GetAllQuestionUsecaseImpl(questionGateway);
     }
 }
