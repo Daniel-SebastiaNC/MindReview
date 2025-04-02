02e5c0cf4e9ad52f949ac9ab100c3d4c08c488e1
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-04-02 17:06:01 -0300

    feat: add findAllByIsNeedReviewOrderByPriorityAsc(boolean b) in QuestionRepository

--- src/main/java/br/com/dev/danielsebastian/MindReview/infra/persistence/QuestionRepository.java
+++ src/main/java/br/com/dev/danielsebastian/MindReview/infra/persistence/QuestionRepository.java
@@ -3,6 +3,10 @@ package br.com.dev.danielsebastian.MindReview.infra.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
+    List<QuestionEntity> findAllByIsNeedReviewOrderByPriorityAsc(boolean b);
+
 }
