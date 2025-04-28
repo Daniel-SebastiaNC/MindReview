16abecf10ff11917ab3668dadb48d6d44a8c66d8
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-04-27 21:19:23 -0300

    test(fix): add EntityManager in CreateQuestionUsecaseTest to Transactional DB

--- src/test/java/br/com/dev/danielsebastian/MindReview/core/usecases/CreateQuestionUsecaseTest.java
+++ src/test/java/br/com/dev/danielsebastian/MindReview/core/usecases/CreateQuestionUsecaseTest.java
@@ -7,6 +7,9 @@ import br.com.dev.danielsebastian.MindReview.core.gateway.QuestionGateway;
 import br.com.dev.danielsebastian.MindReview.infra.gateway.QuestionRepositoryGateway;
 import br.com.dev.danielsebastian.MindReview.infra.mappers.QuestionEntityMapper;
 import br.com.dev.danielsebastian.MindReview.infra.persistence.QuestionRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,13 +32,22 @@ class CreateQuestionUsecaseTest {
 
     private CreateQuestionUsecase createQuestionUsecase;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @BeforeEach
     void setUp() {
         gateway = new QuestionRepositoryGateway(questionRepository, mapper);
         createQuestionUsecase = new CreateQuestionUsecaseImpl(gateway);
     }
 
+    @AfterEach
+    void resetDB() {
+        entityManager.createNativeQuery("TRUNCATE TABLE tb_question RESTART IDENTITY").executeUpdate();
+    }
+
     @Test
+    @Transactional
     void createQuestionSuccess() {
         // Arrange
         Question input = new Question(
