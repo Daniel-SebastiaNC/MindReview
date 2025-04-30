1c3c1398e1afaa0fd2e5734b3df8929bcdd14f63
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-04-30 11:04:24 -0300

    test(feat): add createQuestionEntity(LocalDateTime) to persiste a Question from UpdateQuestionUsecaseTest

---
+++ src/test/java/br/com/dev/danielsebastian/MindReview/core/usecases/UpdateQuestionUsecaseTest.java
@@ -0,0 +1,63 @@
+package br.com.dev.danielsebastian.MindReview.core.usecases;
+
+import br.com.dev.danielsebastian.MindReview.core.domians.Question;
+import br.com.dev.danielsebastian.MindReview.core.enuns.DifficultyQuestion;
+import br.com.dev.danielsebastian.MindReview.core.enuns.TimeDelay;
+import br.com.dev.danielsebastian.MindReview.core.gateway.QuestionGateway;
+import br.com.dev.danielsebastian.MindReview.infra.exeception.DataNotFoundException;
+import br.com.dev.danielsebastian.MindReview.infra.gateway.QuestionRepositoryGateway;
+import br.com.dev.danielsebastian.MindReview.infra.mappers.QuestionEntityMapper;
+import br.com.dev.danielsebastian.MindReview.infra.persistence.QuestionEntity;
+import br.com.dev.danielsebastian.MindReview.infra.persistence.QuestionRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UpdateQuestionUsecaseTest {
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    private QuestionEntityMapper mapper = new QuestionEntityMapper();
+
+    private QuestionGateway gateway;
+
+    private UpdateQuestionUsecase updateQuestionUsecase;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        gateway = new QuestionRepositoryGateway(questionRepository, mapper);
+        updateQuestionUsecase = new UpdateQuestionUsecaseImpl(gateway);
+    }
+
+    @AfterEach
+    void resetDB() {
+        entityManager.createNativeQuery("TRUNCATE TABLE tb_question RESTART IDENTITY").executeUpdate();
+    }
+
+    private void createQuestionEntity(LocalDateTime localDateTime) {
+        QuestionEntity question = new QuestionEntity();
+        question.setText("text test");
+        question.setResponse("response test");
+        question.setDifficultyQuestion(DifficultyQuestion.EASY);
+        question.setTimeDo(localDateTime);
+        question.setTimeDelay(TimeDelay.NOW);
+        question.setPriority(1);
+        question.setNeedReview(true);
+        this.entityManager.persist(question);
+    }
+}
